2281484ac3b97e152ea06cd5c04bff5d79f15800
Yee Chen <n1533512@gmail.com>
2019-03-28 05:00:32 +0800

    input 50% finish

--- Workshop1/js/script.js
+++ Workshop1/js/script.js
@@ -1,13 +1,48 @@
+$(document).ready(function () {
 
-var bookDataFromLocalStorage = [];
-var bookCategoryList = [
-    { text: "資料庫", value: "database", src: "image/database.jpg" },
-    { text: "網際網路", value: "internet", src: "image/internet.jpg" },
-    { text: "應用系統整合", value: "system", src: "image/system.jpg" },
-    { text: "家庭保健", value: "home", src: "image/home.jpg" },
-    { text: "語言", value: "language", src: "image/language.jpg" }
-];
+    var bookDataFromLocalStorage = [];
+    var bookCategoryList = [
+        { text: "資料庫", value: "database", src: "image/database.jpg" },
+        { text: "網際網路", value: "internet", src: "image/internet.jpg" },
+        { text: "應用系統整合", value: "system", src: "image/system.jpg" },
+        { text: "家庭保健", value: "home", src: "image/home.jpg" },
+        { text: "語言", value: "language", src: "image/language.jpg" }
+    ];
+    $(".BookCategory").kendoDropDownList(); //轉換為KendoUI 下拉式選單
+    $(".BookCategory").kendoDropDownList({  //建立相關屬性
+        dataTextField: "text",
+        dataValueField: "value",
+        dataSource: bookCategoryList,
+        //index: 0,
+        //change: onChange
+    });
+    $(".BookName").kendoMaskedTextBox();
+    $(".BookAuthor").kendoMaskedTextBox();
+    $(".BoughtDatepicker").kendoDatePicker({
+        format:"yyyy/MM/dd",
+        dateInput : true,
+        culture:"zh-TW",
+    });
+    $(".DeliveredDatepicker").kendoDatePicker({
+        format:"yyyy/MM/dd",
+        dateInput : true,
+        culture:"zh-TW",
+    });
+    $(".BookPrice").kendoNumericTextBox({
+        min:0, //禁止輸入負數
+        format:kendo.toString("##,#"),//三位一撇
+        
+    });
+    $(".BookAmount").kendoNumericTextBox({
+        min:0, //禁止輸入負數
+        format:kendo.toString("##,#"),//三位一撇
+    });
 
+
+  
+
+
+});
 // 載入書籍資料
 function loadBookData() {
     bookDataFromLocalStorage = JSON.parse(localStorage.getItem('bookData'));
@@ -16,7 +51,6 @@ function loadBookData() {
         localStorage.setItem('bookData', JSON.stringify(bookDataFromLocalStorage));
     }
 }
-
 $(function () {
     loadBookData();
 });
